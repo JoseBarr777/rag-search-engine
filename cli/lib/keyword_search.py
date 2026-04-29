a6e9313ed79c3e494d1bce2a7e98d02c94e04c2d
Jose Barrera <jfbarrera2002@gmail.com>
2026-04-28 21:40:58 -0500

    feat: added use of inverted index
    Updated the InvertedIndex class to load an inverted index from some data source.

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,8 +1,10 @@
-import string
 import os
 import pickle
+import string
 from collections import defaultdict
+
 from nltk.stem import PorterStemmer
+
 from .search_utils import (
     CACHE_DIR,
     DEFAULT_SEARCH_LIMIT,
@@ -10,9 +12,6 @@ from .search_utils import (
 )
 from .stopword_loader import load_stopwords
 
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
-from .stopword_loader import load_stopwords
-from nltk.stem import PorterStemmer
 
 class InvertedIndex:
     def __init__(self) -> None:
@@ -36,6 +35,12 @@ class InvertedIndex:
         with open(self.docmap_path, "wb") as f:
             pickle.dump(self.docmap, f)
 
+    def load(self) -> None:
+        with open(self.index_path, "rb") as f:
+            self.index = pickle.load(f)
+        with open(self.docmap_path, "rb") as f:
+            self.docmap = pickle.load(f)
+
     def get_documents(self, term: str) -> list[int]:
         doc_ids = self.index.get(term, set())
         return sorted(list(doc_ids))
@@ -50,45 +55,24 @@ def build_command() -> None:
     idx = InvertedIndex()
     idx.build()
     idx.save()
-    docs = idx.get_documents("merida")
-    print(f"First document for token 'merida' = {docs[0]}")
+
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
-    movies = load_movies()
-    stop_words = load_stopwords()
-    results = []
-    for movie in movies:
-        query_tokens = filter_stopwords(tokenize_text(query), stop_words)
-        title_tokens = filter_stopwords(tokenize_text(movie["title"]), stop_words)
-        stemmed_query_tokens = stem_tokens(query_tokens)
-        stemmed_title_tokens = stem_tokens(title_tokens)
-        if has_matching_token(stemmed_query_tokens, stemmed_title_tokens):
-            results.append(movie)
+    idx = InvertedIndex()
+    idx.load()
+    query_tokens = tokenize_text(query)
+    seen, results = set(), []
+    for query_token in query_tokens:
+        matching_doc_ids = idx.get_documents(query_token)
+        for doc_id in matching_doc_ids:
+            if doc_id in seen:
+                continue
+            seen.add(doc_id)
+            results.append(idx.docmap[doc_id])
             if len(results) >= limit:
-                break
+                return results
 
     return results
-
-def stem_tokens(tokens: list[str]):
-    stemmer = PorterStemmer()
-    seen = set()
-    for token in tokens:
-        stemmed_token = stemmer.stem(token)
-        if stemmed_token not in seen:
-            seen.add(stemmed_token)
-            
-    return list(seen)
-
-def filter_stopwords(tokens: list[str], stop_words: list[str]) -> list[str]:
-    return [token for token in tokens if token not in stop_words]
-
-
-def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
-    for query_token in query_tokens:
-        for title_token in title_tokens:
-            if query_token in title_token:
-                return True
-    return False
 
 
 def preprocess_text(text: str) -> str:
@@ -99,9 +83,7 @@ def preprocess_text(text: str) -> str:
 
 def tokenize_text(text: str) -> list[str]:
     text = preprocess_text(text)
-    tokens = text.split()
-    valid_tokens = []
-    for token in tokens:
-        if token:
-            valid_tokens.append(token)
-    return valid_tokens
+    valid_tokens = [token for token in text.split() if token]
+    stop_words = load_stopwords()
+    stemmer = PorterStemmer()
+    return [stemmer.stem(word) for word in valid_tokens if word not in stop_words]
