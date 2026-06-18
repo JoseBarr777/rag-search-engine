0eccbadd37250619294ddb6e597c0a5fede47866
Jose Barrera <jfbarrera2002@gmail.com>
2026-06-17 22:11:09 -0500

    feat: Semantic Search command fully implemented
    Users can enter a string query to retrieve top 3 related movies.

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -5,6 +5,7 @@ import argparse
 from lib.semantic_search import (
     embed_query_text,
     embed_text,
+    semantic_search,
     verify_embeddings,
     verify_model,
 )
@@ -30,6 +31,12 @@ def main() -> None:
     )
     embed_query_parser.add_argument("query", type=str, help="Query to embed")
 
+    search_parser = subparsers.add_parser(
+        "search", help="Search for movies by meaning"
+    )
+    search_parser.add_argument("query", type=str, help="Search query")
+    search_parser.add_argument("--limit", type=int, default=5, help="Number of results to return")
+
     args = parser.parse_args()
 
     match args.command:
@@ -41,6 +48,8 @@ def main() -> None:
             verify_embeddings()
         case "embed_query":
             embed_query_text(args.query)
+        case "search":
+            semantic_search(args.query, args.limit)
         case _:
             parser.print_help()
 
